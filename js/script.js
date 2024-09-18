86b8122c0ea4a7640cbfb556044b0d7f6013db71
RaenD28 <rendynurulsaputra28@gmail.com>
2024-09-18 14:13:11 +0700

    nambah medsos

--- js/script.js
+++ js/script.js
@@ -12,8 +12,25 @@ document.querySelector("#hamburger-menu").onclick = () => {
 const hm = document.querySelector("#hamburger-menu");
 
 document.addEventListener("click", function (e) {
-  if (!hm.contains(e.target) && !navMenu.contains(e.target) && !navExtra.contains(e.target)) {
+  if (
+    !hm.contains(e.target) &&
+    !navMenu.contains(e.target) &&
+    !navExtra.contains(e.target)
+  ) {
     navMenu.classList.remove("active");
     navExtra.classList.remove("active");
   }
 });
+
+// Fitur memunculkan pop-up foto galeri
+document.querySelectorAll(".isi-foto-galeri img").forEach((image) => {
+  image.onclick = () => {
+    document.querySelector(".foto-popup").style.display = "block"; // display block supaya terlihat jelas untuk popup
+    document.querySelector(".foto-popup img").src = image.getAttribute("src"); // ambil nilai src supaya gambar yang tampil sesuai dengan gambar yang di klik (karena sudah di looping)
+  };
+});
+
+// Mengeluarkan popup melalui span (tombol x)
+document.querySelector(".foto-popup span").onclick = () => {
+  document.querySelector(".foto-popup").style.display = "none";
+};
