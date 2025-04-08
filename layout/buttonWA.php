5fb4447f0f86f3c11739cb99e9bf61f431c8fa59
RaenD28 <rendynurulsaputra28@gmail.com>
2025-04-09 00:29:21 +0700

    kembali ke animasi button

--- layout/buttonWA.php
+++
@@ -1,4 +0,0 @@
-<a href="https://wa.me/6281389222322">
-    <button type="submit" class="button-wa" data-aos="fade-down" data-aos-delay="200"> Konsultasi via Whatsapp
-        <img src="icon/whatsapp.png" alt=""> </button>
-</a>
